8ad71291306635479da7a59db426c8c32f6b6cbc
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-04 08:56:13 -0700

    Import Kivy widgets and Translator module

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.spinner import Spinner
+from kivy.uix.boxlayout import BoxLayout
+from translate import Translator
